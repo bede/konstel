eb648e22c85783e381719239d0f42b5833af7f06
Bede Constantinides <1007616+bede@users.noreply.github.com>
2021-02-03 16:15:37 +0000

    0.3.1: Catch SARS2 S:L5F, sanity check spike length

--- konstel/konstel.py
+++ konstel/konstel.py
@@ -111,12 +111,13 @@ def sars2_nuc_to_spike_prot(nuc_sequence):
     '''Returns translated SARS-CoV-2 spike contained in nucleotide string'''
     nuc_sequence_fmt = nuc_sequence.translate(str.maketrans('', '', ' \n\t\r'))
     seq = Seq(nuc_sequence_fmt)
-    s_start_seq = 'atgtttgtttttctt'  # First 5 codons of Wuhan-Hu-1
+    s_start_seq = 'atgtttgttttt'  # First 4 codons of Wuhan-Hu-1
     s_end_seq = 'ttacattacacataa'  # Last 5 codons of Wuhan-Hu-1
     s_start_pos = str(seq).lower().index(s_start_seq)
     s_end_pos = str(seq).lower().index(s_end_seq) + len(s_end_seq)
     spike_nucl = seq[s_start_pos:s_end_pos]
     spike_prot = str(spike_nucl.ungap().translate()).strip('*')
+    assert(1200 < len(spike_prot) < 1300)  # SARS-CoV-2 is ~1270AAs long 
     return spike_prot
 
 def sars2_nuc_fasta_to_spike_prot(fasta_path):
